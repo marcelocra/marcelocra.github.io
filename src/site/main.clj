61f54ab4fe023fb01ede1911a85f50acb4b4ebb9
Marcelo Almeida <marcelocralmeida@gmail.com>
2020-09-01 15:52:00 -0300

    Add some tests that I'll revert shortly

--- src/site/main.clj
+++ src/site/main.clj
@@ -4,7 +4,11 @@
   (:import [org.asciidoctor Asciidoctor$Factory SafeMode]
            org.asciidoctor.jruby.AsciiDocDirectoryWalker))
 
-(defn main [])
+;; (defn -main [])
+
+(import '[java.nio.file WatchService FileSystem FileSystems StandardWatchEventKinds])
+(def watcher (.. FileSystems (getDefault) (newWatchService)))
+(def watch-key)
 
 (comment
   ;; Convert all docs in content/** from adoc to html
@@ -14,5 +18,8 @@
                      (AsciiDocDirectoryWalker. "content/pages")
                      {"safe" (.getLevel SafeMode/SAFE)
                       "to_dir" "out/content"
-                      "mkdirs" "true"})
+                      "mkdirs" "true"
+                      "backend" "html"})
   (filter #(.isFile %) (file-seq (io/file "content/pages"))))
+
+(System/getProperty "user.dir")
